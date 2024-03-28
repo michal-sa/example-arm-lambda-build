6061dac188532b60d775e2f715d023605a98016a
Michal Sadowski <michal.sadowski@axis.com>
2024-03-28 16:53:52 +0100

    Run pki cert example

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,10 +1,28 @@
 import json
 
-from pki_tools import Name, Certificate, LoadError
+import datetime
+
+from pki_tools import Name, Certificate, Validity, RSAKeyPair, SHA512
 
 
 def lambda_handler(event, context):
-    print(pki_tools.__version__)
+    name = Name(cn=["Cert CN"])
+
+    validity = Validity(
+        not_before=datetime.datetime.today() - datetime.timedelta(days=1),
+        not_after=datetime.datetime.today() + datetime.timedelta(days=1),
+    )
+
+    cert = Certificate(
+        subject=name,
+        issuer=name,
+        validity=validity,
+    )
+
+    cert.sign(RSAKeyPair.generate(), SHA512)
+
+    print(cert)
+
 
     # try:
     #     ip = requests.get("http://checkip.amazonaws.com/")
